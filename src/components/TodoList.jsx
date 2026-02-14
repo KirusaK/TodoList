c9dc1f428cb76eb5f80d9144596c743b22fa0ff2
KirusaK <kirusaklimenko@gmail.com>
2026-02-14 21:07:21 +0100

    perf: optimize renders using memo, useCallback and useMemo

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import TodoItem from "./TodoItem.jsx";
 
 const TodoList = (props) => {
@@ -41,4 +42,4 @@ const TodoList = (props) => {
   );
 };
 
-export default TodoList;
+export default memo(TodoList);
