fc323b6ba6fdae6f16fe07389fea9cb6c112df19
KirusaK <kirusaklimenko@gmail.com>
2026-03-12 17:45:14 +0100

    fix: correct Vite base config and localStorage API

--- src/app/routing/Router.jsx
+++ src/app/routing/Router.jsx
@@ -10,21 +10,21 @@ const getCurrentPath = () => {
 };
 
 const matchPath = (path, route) => {
-  const pathParts = path.split("/"); // '/tasks/123' => ['', 'tasks', '123']
-  const routePaths = route.split("/"); // '/tasks/:id' => ['', 'tasks', ':id']
+  const pathParts = path.split("/");
+  const routeParts = route.split("/");
 
-  if (pathParts.length !== routePaths.length) {
+  if (pathParts.length !== routeParts.length) {
     return null;
   }
 
   const params = {};
 
-  for (let i = 0; i < routePaths.length; i++) {
-    if (routePaths[i].startsWith(":")) {
-      const paramName = routePaths[i].slice(1);
+  for (let i = 0; i < routeParts.length; i++) {
+    if (routeParts[i].startsWith(":")) {
+      const paramName = routeParts[i].slice(1);
 
       params[paramName] = pathParts[i];
-    } else if (routePaths[i] !== pathParts[i]) {
+    } else if (routeParts[i] !== pathParts[i]) {
       return null;
     }
   }
@@ -45,7 +45,7 @@ export const useRoute = () => {
     return () => {
       window.removeEventListener("popstate", onLocationChange);
     };
-  });
+  }, []);
 
   return path;
 };
